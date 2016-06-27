ca3c18727a789019957b2aff112702278c188d72
Ing. Enrique Ugalde <eugalde@ing-enrique-ugalde-macbook-pro.local>
2016-06-26 21:37:51 -0500

    Initial Commit

---
+++ 1appWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,35 @@
+//
+//  InterfaceController.swift
+//  1appWatch WatchKit Extension
+//
+//  Created by Ing. Enrique Ugalde on 26/06/16.
+//  Copyright © 2016 Ing. Enrique Ugalde. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet var despliegue: WKInterfaceLabel!
+    @IBAction func Saludar() {
+        self.despliegue.setText("Hola Mundo!")
+    }
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ 1appWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1appWatch
+//
+//  Created by Ing. Enrique Ugalde on 26/06/16.
+//  Copyright © 2016 Ing. Enrique Ugalde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
